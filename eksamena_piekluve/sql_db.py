d3419cdd64f91a32301d723be7a42a06a8c379a5
Jank0ss <111281359+Jank0ss@users.noreply.github.com>
2023-02-21 08:43:35 +0200

    Add files via upload

--- eksamena_piekluve/sql_db.py
+++ eksamena_piekluve/sql_db.py
@@ -2,4 +2,4 @@ import PySimpleGUI as sg
 
 headings = ['Detaļas izmēri', 'Darbība', 'Daudzums', 'Koeficients', 'Gala summa']
 
-lay
+layout = [[sg.text("Ievadi darbību:"), sg.Input(key='-DARBIBA-', size=(20, 1))]]
