3ee1a777afc1b0c34dd45e4faca3d267f2c72b3a
Jank0ss <111281359+Jank0ss@users.noreply.github.com>
2023-02-28 10:26:16 +0200

    Add files via upload
    gANDRIZ PABEIGTS

--- eksamena_piekluve/sql_db.py
+++ eksamena_piekluve/sql_db.py
@@ -1,12 +1,11 @@
 import PySimpleGUI as sg
 
-headings = ['Detaļas izmēri', 'Darbība', ['Ēvelēšana', 'Garināšana', 'Zāģēšana'], 'Daudzums', 'Koeficients', 'Gala summa']
-
 class Logs:
 
     layout = [
-        [sg.Text("Ievadi izmērus:"), sg.I(key = '-GARUMS-'), sg.I(key = '-PLATUMS-')],
-        [sg.T('Ievadīts: ')], [sg.T(key='-OUT_GAR-'), sg.T(key='-OUT_PLAT-')],
+        [sg.Text("Ievadi izmērus:"), sg.I(key = '-GARUMS-',enable_events=True,size=(5, 1)), sg.I(key = '-PLATUMS-',enable_events=True,size=(5, 1))], 
+        [sg.Text("Izvēlies darbību:"),sg.OptionMenu(values = ['Ēvelēšana', 'Garināšana', 'Zāģēšana'],default_value = 'Ēvelēšana', key = "-DROPS-"),sg.Text("Ievadi daudzumu:"),sg.I(key = '-DAUDZUMS-', size = (5,1))],
+        [sg.T('Ievadīts: ')], [sg.T(key='-OUT_GAR-'), sg.T(key='-OUT_PLAT-'),sg.T(key='-OUT_DROPS-'),sg.T(key='-OUT_DAUDZ-')],
         [sg.B('Ievadīt'),sg.Exit()]
     ]
 
@@ -16,10 +15,21 @@ class Logs:
     while True:
         event, values = window.read()
         # print(event, values)  # izdrukā bibliotēkas vērtības
+        
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
         if event == 'Ievadīt':
             window['-OUT_GAR-'].update(values['-GARUMS-'])
             window['-OUT_PLAT-'].update(values['-PLATUMS-'])
+            window['-OUT_DROPS-'].update(values['-DROPS-'])
+            window['-OUT_DAUDZ-'].update(values['-DAUDZUMS-'])
+        if event == '-GARUMS-' and values['-GARUMS-']:
+            in_as_float = float(values['-GARUMS-'])
+            if float(values['-GARUMS-']) == 0:
+                window['-GARUMS-'].update('1')
+        if event == '-PLATUMS-' and values['-PLATUMS-']:
+            in_as_float = float(values['-PLATUMS-'])
+            if float(values['-PLATUMS-']) == 0:
+                window['-PLATUMS-'].update('1')
 
     window.close()
