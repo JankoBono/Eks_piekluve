521cfbe756b2dbf33274cf273bf6f6e6c52d673a
Jank0ss <111281359+Jank0ss@users.noreply.github.com>
2023-02-28 08:58:02 +0200

    Add files via upload
    gm, strada

--- eksamena_piekluve/sql_db.py
+++ eksamena_piekluve/sql_db.py
@@ -1,5 +1,25 @@
 import PySimpleGUI as sg
 
-headings = ['Detaļas izmēri', 'Darbība', 'Daudzums', 'Koeficients', 'Gala summa']
+headings = ['Detaļas izmēri', 'Darbība', ['Ēvelēšana', 'Garināšana', 'Zāģēšana'], 'Daudzums', 'Koeficients', 'Gala summa']
 
-layout = [[sg.text("Ievadi darbību:"), sg.Input(key='-DARBIBA-', size=(20, 1))]]
+class Logs:
+
+    layout = [
+        [sg.Text("Ievadi izmērus:"), sg.I(key = '-GARUMS-'), sg.I(key = '-PLATUMS-')],
+        [sg.T('Ievadīts: ')], [sg.T(key='-OUT_GAR-'), sg.T(key='-OUT_PLAT-')],
+        [sg.B('Ievadīt'),sg.Exit()]
+    ]
+
+    window = sg.Window("gmgm", layout)   
+
+
+    while True:
+        event, values = window.read()
+        # print(event, values)  # izdrukā bibliotēkas vērtības
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            break
+        if event == 'Ievadīt':
+            window['-OUT_GAR-'].update(values['-GARUMS-'])
+            window['-OUT_PLAT-'].update(values['-PLATUMS-'])
+
+    window.close()
